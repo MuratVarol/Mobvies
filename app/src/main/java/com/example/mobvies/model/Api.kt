db06c30ae07d2e52846702296a26d4d636f366ec
varol <murat.varol@kgteknoloji.com>
2019-02-09 21:27:01 +0300

    movies methods modified to single method

--- app/src/main/java/com/example/mobvies/model/Api.kt
+++ app/src/main/java/com/example/mobvies/model/Api.kt
@@ -4,30 +4,16 @@ import com.example.mobvies.remote.BaseMoviesResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface Api {
 
-    @GET("popular")
-    fun getPopularMovies(
+    @GET
+    fun getMovies(
+        @Url() url: String,
         @Query(value = "api_key") apiKey: String,
+        @Query(value = "page") page: Int,
         @Query(value = "language") language: String,
-        @Query(value = "page") page: String,
-        @Query(value = "region") region: String
-    ): Single<BaseMoviesResponse<MoviesModel>>
-
-    @GET("latest")
-    fun getLatestMovies(
-        @Query(value = "api_key") apiKey: String,
-        @Query(value = "language") language: String,
-        @Query(value = "page") page: String,
-        @Query(value = "region") region: String
-    ): Single<BaseMoviesResponse<MoviesModel>>
-
-    @GET("upcoming")
-    fun getUpcomingMovies(
-        @Query(value = "api_key") apiKey: String,
-        @Query(value = "language") language: String,
-        @Query(value = "page") page: String,
         @Query(value = "region") region: String
     ): Single<BaseMoviesResponse<MoviesModel>>
 
