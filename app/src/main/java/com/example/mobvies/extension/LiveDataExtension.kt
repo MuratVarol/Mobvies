c39616b19bf1c02c68cba84f8abeb3d7c699e058
varol <murat.varol@kgteknoloji.com>
2019-02-09 22:47:30 +0300

    operator override method for plus on MLD added

--- app/src/main/java/com/example/mobvies/extension/LiveDataExtension.kt
+++ app/src/main/java/com/example/mobvies/extension/LiveDataExtension.kt
@@ -1,7 +1,14 @@
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 
 inline fun <T> LiveData<T>.observe(owner: LifecycleOwner, crossinline observer: (t: T?) -> Unit) {
     observe(owner, Observer { observer(it) })
+}
+
+operator fun <T> MutableLiveData<ArrayList<T>>.plusAssign(values: List<T>) {
+    val value = this.value ?: arrayListOf()
+    value.addAll(values)
+    this.value = value
 }
