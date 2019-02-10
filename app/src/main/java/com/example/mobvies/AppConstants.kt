bc769a4f4e1f336c934c6c0706cb3e9cf52dc689
varol <murat.varol@kgteknoloji.com>
2019-02-10 21:17:28 +0300

    base image link added

--- app/src/main/java/com/example/mobvies/AppConstants.kt
+++ app/src/main/java/com/example/mobvies/AppConstants.kt
@@ -1,5 +1,6 @@
 package com.example.mobvies
 
 const val BASE_LINK = "https://api.themoviedb.org/3/movie/"
+const val BASE_IMAGE_LINK = "https://image.tmdb.org/t/p/original"
 
 const val APP_DATABASE_NAME = "db.mobvies"
