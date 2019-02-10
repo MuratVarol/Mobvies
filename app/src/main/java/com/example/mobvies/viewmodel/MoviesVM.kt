d327d485785e052443dc9489bb7ae630eaed5636
varol <murat.varol@kgteknoloji.com>
2019-02-10 21:23:04 +0300

    iten click listener modified

--- app/src/main/java/com/example/mobvies/viewmodel/MoviesVM.kt
+++ app/src/main/java/com/example/mobvies/viewmodel/MoviesVM.kt
@@ -7,6 +7,7 @@ import com.example.mobvies.model.MoviesModel
 import com.example.mobvies.remote.DataHolder
 import com.example.mobvies.remote.enums.MovieType
 import com.example.mobvies.usecase.GetMoviesUseCase
+import com.example.mobvies.util.SingleLiveEvent
 import com.example.mobvies.util.listener.ItemClickListener
 import plusAssign
 
@@ -14,19 +15,22 @@ class MoviesVM(
     private val getMoviesUseCase: GetMoviesUseCase
 ) : BaseVM() {
 
-    private val popularMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
-    private val upcomingMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
-    private val latestMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
+    val popularMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
+    val upcomingMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
+    val latestMovieViewEntityList = MutableLiveData<ArrayList<MoviesModel>>()
+
+    val selectedMovie = SingleLiveEvent<MoviesModel>()
 
 
-    val itemClickListener = object : ItemClickListener<Int> {
-        override fun onItemClick(view: View, item: Int, position: Int) {
-
+    val itemClickListener = object : ItemClickListener<MoviesModel> {
+        override fun onItemClick(view: View, item: MoviesModel, position: Int) {
+            selectedMovie.postValue(item)
         }
+
     }
 
     init {
-
+        getPopularMovies(1)
     }
 
     fun getPopularMovies(page: Int) {
@@ -48,7 +52,7 @@ class MoviesVM(
         val disposable = getMoviesUseCase
             .getMovies(movieType, page)
             .subscribeOn(getBackgroundScheduler())
-            .observeOn(getBackgroundScheduler())
+            .observeOn(getMainThreadScheduler())
             .subscribe { data ->
                 isLoading.postValue(true)
 
